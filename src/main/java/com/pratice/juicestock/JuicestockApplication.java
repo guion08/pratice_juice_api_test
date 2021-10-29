316b361d6354a0ee725ea7e84ba5cc396e95cc13
Guilherme de Araujo Jose <guion2717@gmail.com>
2021-10-29 15:50:40 -0300

    Aplicação de recursos iniciais

---
+++ src/main/java/com/pratice/juicestock/JuicestockApplication.java
@@ -0,0 +1,11 @@
+package com.pratice.juicestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuicestockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JuicestockApplication.class, args);
+    }
+}
